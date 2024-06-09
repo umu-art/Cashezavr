6673787843524a3b73a05cbf82a26c2e387631fe
Казенин Владимир <kazeninvladimir@gmail.com>
2024-06-09 16:25:32 +0300

    new security config

--- cashezavr-iac/utils/cashezavr-load-recovery/main.py
+++ cashezavr-iac/utils/cashezavr-load-recovery/main.py
@@ -1,7 +1,8 @@
 import pika
+import os
 
 if __name__ == '__main__':
-    credentials = pika.PlainCredentials('rmuser', 'rmpassword')
+    credentials = pika.PlainCredentials(os.environ['RABBIT_USER'], os.environ['RABBIT_PASSWORD'])
     parameters = pika.ConnectionParameters('rabbit.cashezavr-db', 5672, '/', credentials)
     connection = pika.BlockingConnection(parameters)
     channel = connection.channel()
